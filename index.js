5a720a0bd032fbdc23c881dfc5820fa8f8e0594c
Amnesic Oblex <taylor@Taylors-MacBook-Pro.local>
2019-12-09 16:32:27 -0500

    Added rollObjects and rollWeightedObject functions

--- index.js
+++ index.js
@@ -100,6 +100,49 @@ module.exports = {
             delete element.weight
             return element
         }
+    },
+
+    /**
+     * Randomly selects a prop object with a value weight from a parent object.
+     * @param {Object} object The parent object with weighted props.
+     * @param {Number} [diceNum] The number of dice to roll. Optional.
+     * @param {Number} diceType The type of dice to roll. Optional.
+     * @returns Object with the prop name and the prop object with the weight stripped out.
+     */
+    rollWeightedObject (object, diceNum, diceType) {
+        // Converts the object to a weighted table
+        var table = []
+        for (prop in object) {
+            table.push({
+                weight: object[props].value,
+                value: prop
+            })
+        }
+        // Randomly selects a prop using the rollWeightedTable function 
+        var result = {}
+        result.prop = this.rollWeightedTable(table, diceNum, diceType)
+        result.object = object[result.prop]
+        delete result.object.value
+        return result
+    },
+
+    /**
+     * Randomly selects a prop from a parent object.
+     * @param {Object} object The parent object.
+     * @returns The selected prop name and value
+     */
+    rollObject (object) {
+        // Converts the object to a table
+        var table = []
+        for (props in object) {
+            table.push(props)
+        }
+        // Randomly selects a prop using the rollTable function 
+        var result = {}
+        result.prop = this.rollTable(table)
+        result.object = object[result.prop]
+        delete result.object.value
+        return result
     }
 }
 
